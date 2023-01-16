a03192e24522fe4e80b25038737925754c94ae30
Sergei Keler <sergei@keler.net>
2023-01-16 21:58:27 +0300

    Add files via upload

---
+++ take_shot.py
@@ -0,0 +1,41 @@
+#!/usr/local/bin/python3
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+
+opts = Options()
+opts.add_argument("--headless")
+print("Запускаю Хром в безголовлом режиме.")
+driver = webdriver.Chrome(options=opts)
+driver.set_window_size(1024, 1024)
+print("Запустил в размере 1Kx1K.")
+url = "https://ya.ru"
+delay = 3 # seconds
+print(f"Даю запрос {url} и жду {delay} секунд.")
+driver.get(url)
+print("Запрос улетел.")
+try:
+    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'text')))
+    print(f"Страница {url} загружена.")
+except TimeoutException:
+    print(f"Не удалось загрузить {url} за {delay} секунд.")
+driver.save_screenshot("screenshot0.png")
+print("Заскриншотил 0.")
+driver.find_element(By.NAME, 'text').send_keys('жопа стряслась')
+submit = driver.find_element(By.XPATH, '/html/body/main/div[2]/form/div[2]/button')
+print("Написал и нашёл, куда жать.")
+submit.click()
+print("Жмакнул.")
+try:
+    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'text')))
+    print(f"Кликнул успешно.")
+except TimeoutException:
+    print(f"Не накликал за {delay} секунд.")
+driver.save_screenshot("screenshot1.png")
+print("Заскриншотил 1.")
+driver.quit()
+print("Кончил.")
